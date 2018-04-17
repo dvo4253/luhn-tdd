3ee65d439e5512d5fbcea75d7ae15633b5a1ddf5
dvo4253 <dvo4253.miscaccounts@gmail.com>
2018-04-17 15:41:20 -0500

    Testing output examples

--- app/index.test.js
+++ app/index.test.js
@@ -1,6 +1,11 @@
 import luhnTest from '.'
 import { expect } from 'chai';
-
+// Examples:
+// 125: true
+// 1685: true
+// 98764: true
+// 12345678903: true
+// 831 :false
 describe('luhn tests', () => {
 
 })
