6eeb4fb5da234e468f4e4b2130cdb75f7bbd735a
Nathan Dvorachek <dvo4253.miscaccounts@gmail.com>
2018-07-03 11:53:47 -0500

    git push origin group-1

--- app/index.js
+++ app/index.js
@@ -3,18 +3,30 @@
 
 // 1. -- num < 0 -> return false
 // 2. -- num # digits > 16 --> return false
-// 3. -- Add the numbers, 
-//    --    from the right add the digits 
+// 3. -- from the right add the digits, 
 //    --    double every second digit, 
 //    --    if the doubled digit is >= 10 then add the two digits to get the sum (i.e. 7 + 7 = 14 --> 5)
+//    --     
 // 4. -- After all numbers have been summed, if the sum is divisible by 10 then return true
 // 5. -- Else, return false
 
 
 // example: num = 174
 // 4 + (7 -- 14 = 5) + 1 = 10 which is divisible by 10
-export default (num) => {
+export default num => (num < 0 || num.toString().length > 16 || luhnSum(num) % 10 !== 0) ? false : true
 
-    return false
+export function luhnSum(num) {
+    let sum = 0;
+    num.toString().split('').reverse().forEach((element, index) => {
+        const parsedInt = Number(element)
+        if (index % 2 === 0) {
+            sum += parsedInt
+        } else {
+            (parsedInt * 2).toString().split('').forEach(element => {
+                sum += Number(element);
+            })
+        }
+    });
+    return sum;
 }
 
--- app/index.test.js
+++ app/index.test.js
@@ -1,6 +1,41 @@
-import luhnTest from '.'
+import luhnTest, { luhnSum } from '.';
 import { expect } from 'chai';
 
-describe('luhn tests', () => {
+describe('luhn sum', () => {
+    it('should return 7 for (15)', () => {
+        expect(luhnSum(15)).to.equal(7);
+    })
 
+    it('should return 2 for (51)', () => {
+        expect(luhnSum(51)).to.equal(2);
+    })
+
+    it('should return 29 for (927143)', () => {
+        expect(luhnSum(927143)).to.equal(28);
+    })
+
+    it('should return ??? for (abc)', () => {
+        expect(Number.isNaN(luhnSum('abc'))).to.equal(true);
+    })
+})
+
+describe('luhn tests', () => {
+    it('should return false if num < 0', () => {
+        expect(luhnTest(-1)).to.equal(false);
+    })
+    it('should return false if #num > 16', () => {
+        expect(luhnTest(11111111111111111)).to.equal(false);
+    })
+
+    it('should return true if num is divisible by 10', () => {
+        expect(luhnTest(4368020935033742)).to.equal(true);
+    })
+
+    it('should return true if num is divisible by 10 (16 digit)', () => {
+        expect(luhnTest(174)).to.equal(true);
+    })
+
+    it('should return false if num is not divisible by 10', () => {
+        expect(luhnTest(1437486333098805)).to.equal(false);
+    })
 })
